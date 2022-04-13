175be2f662ad061b60ede2bcf2fb5fabe981c688
shadi1997 <shady@cato.com.lb>
2022-04-13 23:04:19 +0300

    Product class

---
+++ widget/common/classes/product.js
@@ -0,0 +1,13 @@
+class Product {
+    constructor() {
+        this.title = "";
+        this.subTitle = "";
+        this.description = "";
+        this.profileImgUrl = "";
+        this.coverImgUrl = "";
+    }
+}
+
+const Products = {
+    
+}
