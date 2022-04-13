e4fc337bc514bf755d4244ef4a681c76b1632108
pierrekamel5 <pierrekamel57@yahoo.com>
2022-04-13 23:33:20 +0300

    Added product functionalities

--- widget/common/classes/product.js
+++ widget/common/classes/product.js
@@ -5,9 +5,34 @@ class Product {
         this.description = "";
         this.profileImgUrl = "";
         this.coverImgUrl = "";
+        this.creationDate = new Date();
     }
 }
 
 const Products = {
-    
+    insert:(product,callback)=>{
+        if(product.title == "" || product.profileImgUrl == "" || product.coverImgUrl == ""){
+            callback(null,false);
+            return;
+        }
+        buildfire.datastore.insert(product, "Products", callback);
+        callback(null, true)
+    } 
+    ,search:(options,callback)=>{
+        buildfire.datastore.search(options,"Products",callback)
+    }
+    ,getById:(productId, callback)=>{
+        buildfire.datastore.getById(productId,"Products",callback)
+        callback(null,true)
+
+    }
+    ,update:(product,callback)=>{
+        buildfire.datastore.update(product,"Products",callback)
+        callback(null,true)
+    }
+    ,delete:(productId,callback)=>{
+        buildfire.datastore.delete(productId,"Products",callback)
+        callback(null,true)
+    }
+
 }
