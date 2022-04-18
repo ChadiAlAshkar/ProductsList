93e2fcd5dfd212531ace3b5b01961aaa7a0494f1
pierrekamel5 <pierrekamel57@yahoo.com>
2022-04-18 14:49:47 +0300

    Added javascript files for introduction pages in widget and content

---
+++ control/introduction/app.js
@@ -0,0 +1,92 @@
+let editor = new buildfire.components.carousel.editor(".carousel", []);
+let introduction = new IntroductionItem();
+introduction.description = "";
+introduction.images = [];
+
+function init() {
+  Introduction.get()
+    .then((result) => {
+      if (result) {
+        introduction.description = result.data.description;
+        let timer;
+        tinymce.init({
+          selector: "#wysiwygContent",
+          oninit: "postInitWork",
+          setup: function (editor) {
+            editor.on("init", function (e) {
+              tinymce
+                .get("wysiwygContent")
+                .setContent(introduction.description);
+            });
+            editor.on("keyup", function (e) {
+              clearTimeout(timer);
+              timer = setTimeout(() => {
+                save();
+              }, 500);
+            });
+            editor.on("change", function (e) {
+              clearTimeout(timer);
+              timer = setTimeout(() => {
+                save();
+              }, 500);
+            });
+          },
+        });
+
+        introduction.images = result.data.images;
+        editor.loadItems(introduction.images);
+      }
+    })
+    .catch((err) => {
+      console.error("Error in getting Introduction::: ", err);
+    });
+  //Carousel Listeners and functions
+  editor.onItemChange = (item, index) => {
+    if (introduction.images.length > index) {
+      introduction.images[index].title = item.title;
+      introduction.images[index].action = item.action;
+      introduction.images[index].iconUrl = item.iconUrl;
+      save();
+    }
+  };
+
+  editor.onOrderChange = (item, oldIndex, newIndex) => {
+    reOrderCarousel(oldIndex, newIndex);
+    save();
+  };
+
+  editor.onAddItems = (items) => {
+    items.forEach((itm) => {
+      introduction.images.push(itm);
+      save();
+    });
+  };
+
+  editor.onDeleteItem = (item, index) => {
+    introduction.images.splice(index, 1);
+    save();
+  };
+}
+
+function reOrderCarousel(from, to) {
+  // Delete the item from it's current position
+  var item = introduction.images.splice(from, 1);
+  // Make sure there's an item to move
+  if (!item.length) {
+    throw new Error("There is no item in the array at index " + from);
+  }
+  // Move the item to its new position
+  introduction.images.splice(to, 0, item[0]);
+}
+
+function save() {
+  introduction.description = tinymce.activeEditor.getContent();
+
+  Introduction.save(introduction)
+    .then((result) => {})
+    .catch((err2) => {
+      console.error("Error in saving Introduction::: ", err2);
+    });
+}
+
+init();
---
+++ widget/JS/app.js
@@ -0,0 +1,31 @@
+let viewer = new buildfire.components.carousel.view(".carousel");
+let description = document.getElementById("my_container_div");
+
+function init() {
+  this.getIntroduction();
+
+  buildfire.datastore.onUpdate((response) => {
+    if (response.tag == Helper.Collections.PRODUCTS) {
+      console.log("Products updated");
+    }
+    if (response.tag == Helper.Collections.INTRODUCTION) {
+      description.innerHTML = response.data.description;
+      viewer.loadItems(response.data.images);
+    }
+  });
+}
+
+function getIntroduction() {
+  Introduction.get()
+    .then((result) => {
+      if (result) {
+        viewer.loadItems(result.data.images);
+        description.innerHTML = result.data.description;
+      }
+    })
+    .catch((err) => {
+      console.error("Error in getting Introduction::: ", err);
+    });
+}
+
+init();
