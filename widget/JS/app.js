676146e0ada0bd7a3c939f5358e7afe9d09af61e
pierrekamel5 <pierrekamel57@yahoo.com>
2022-04-25 19:17:37 +0300

    changes in listview

--- widget/JS/app.js
+++ widget/JS/app.js
@@ -17,18 +17,21 @@ function init() {
     }
   });
 }
+
+skipIndex = 0;
+limit = 10;
 function getData() {
   var searchOptions = {
     filter: {},
     sort: {},
-    skip: 0,
-    limit: 20,
-    fields: ["id", "title", "profileImgUrl", "subtitle", "description"],
+    skip: this.skipIndex,
+    limit: this.limit,
   };
-
+  console.log(this.limit);
   var promise1 = Introduction.get();
   var promise2 = Products.search(searchOptions);
   Promise.all([promise1, promise2]).then((results) => {
+    console.log(results[1]);
     products = [];
     results[1].forEach((element) => {
       var t = new ListViewItem();
@@ -40,7 +43,9 @@ function getData() {
       t.data = element.data;
       products.push(t);
     });
-    console.log(listView.items);
+    if (results[1].length < this.limit) {
+      isLoadingProductsDone = false;
+    }
     viewer.loadItems(results[0].data.images);
     description.innerHTML = results[0].data.description;
 
@@ -51,6 +56,58 @@ function getData() {
       })
       .catch((err) => {});
   });
+  let t = this;
+  listViewContainer.onscroll = (e) => {
+    if (
+      t.listViewContainer.scrollTop / t.listViewContainer.scrollHeight >
+      0.8
+    ) {
+      this._fetchNextPage();
+    }
+  };
+}
+var endReached = false;
+var fetchingNextPage = false;
+
+function _fetchNextPage() {
+  console.log(this.fetchingNextPage);
+  if (this.fetchingNextPage) return;
+  this.fetchingNextPage = true;
+
+  getNextData(() => {
+    console.log("HIIII");
+    this.fetchingNextPage = false;
+  });
 }
 
+function getNextData(callback) {
+  console.log(endReached);
+  if (this.skipIndex > 0 && this.endReached) return;
+  skipIndex++;
+  console.log(skipIndex);
+
+  var searchOptions = {
+    filter: {},
+    sort: {},
+    skip: this.skipIndex * this.limit,
+    limit: this.limit,
+    fields: ["id", "title", "profileImgUrl", "subtitle", "description"],
+  };
+
+  Products.search(searchOptions).then((result) => {
+    result.forEach((element) => {
+      var t = new ListViewItem();
+      t.id = element.id;
+      t.title = element.data.title;
+      t.description = element.data.description;
+      t.imageUrl = element.data.profileImgUrl;
+      t.subTitle = element.data.subTitle;
+      t.data = element.data;
+      listView.addItem(t);
+    });
+    console.log(result.length);
+    this.endReached = result.length < this.limit;
+    callback();
+  });
+}
 init();
