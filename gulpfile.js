d23d4b665806c4c1de2dd5f4f6265a9f44a9c62f
shadi1997 <shady@cato.com.lb>
2022-05-04 17:13:02 +0300

    widget gulp

--- gulpfile.js
+++ gulpfile.js
@@ -51,15 +51,74 @@ function minifyStrings() {
         .pipe(dest('dist/control/strings'));
 }
 
+function minifyWidget() {
+    return src([
+            'widget/JS/app.js'
+        ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(terser({
+            toplevel: true
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest('dist/widget'));
+}
+
+function minifyCommonW() {
+    return src(
+            [
+                'widget/common/models/product.js',
+                'widget/common/models/language.js',
+                'widget/common/models/introduction.js',
+                'widget/common/controllers/product.js',
+                'widget/common/controllers/language.js',
+                'widget/common/controllers/introduction.js',
+                'widget/common/helper/constants.js',
+                'widget/common/helper/enum.js',
+                'widget/common/helper/ui.js',
+                'widget/common/repository/analytics.js',
+                'widget/common/repository/localnotification.js',
+                'widget/common/repository/pushnotification.js',
+                'widget/common/repository/strings.js',
+                'widget/common/repository/stringsConfig.js'
+            ])
+        .pipe(sourcemaps.init())
+        .pipe(concat('main.js'))
+        .pipe(terser({
+            toplevel: true
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(dest('dist/control/content'));
+}
+
 function watchChanges() {
-    watch('control/content/JS/*.js', minifyContent)
-    watch('control/introduction/app.js', minifyIntro)
-    watch('control/strings/JS/*.js', minifyStrings)
+    watch('control/content/JS/*.js', minifyContent);
+    watch('control/introduction/app.js', minifyIntro);
+    watch('control/strings/JS/*.js', minifyStrings);
+    watch('widget/JS/app.js', minifyWidget);
+    watch([
+        'widget/common/models/product.js',
+        'widget/common/models/language.js',
+        'widget/common/models/introduction.js',
+        'widget/common/controllers/product.js',
+        'widget/common/controllers/language.js',
+        'widget/common/controllers/introduction.js',
+        'widget/common/helper/constants.js',
+        'widget/common/helper/enum.js',
+        'widget/common/helper/ui.js',
+        'widget/common/repository/analytics.js',
+        'widget/common/repository/localnotification.js',
+        'widget/common/repository/pushnotification.js',
+        'widget/common/repository/strings.js',
+        'widget/common/repository/stringsConfig.js'
+    ], minifyCommonW);
 }
 
 exports.default = series([
     minifyContent,
     minifyIntro,
     minifyStrings,
+    minifyWidget,
+    minifyCommonW,
     watchChanges
 ]);
